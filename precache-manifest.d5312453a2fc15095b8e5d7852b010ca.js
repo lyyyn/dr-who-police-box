52dd6345869cc4adbb8fe70965b74dcb74eafd6c
Linawaty <linawatylim@gmail.com>
2020-07-16 21:36:07 +0800

    Updates

---
+++ precache-manifest.d5312453a2fc15095b8e5d7852b010ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2aa3b28a7ba532a29e6c82d750a0440",
+    "url": "/dr-who-police-box/index.html"
+  },
+  {
+    "revision": "f4be93280fcb7a69cfcd",
+    "url": "/dr-who-police-box/static/css/main.d9e1baed.chunk.css"
+  },
+  {
+    "revision": "e022c8b165dd924a0279",
+    "url": "/dr-who-police-box/static/js/2.9ddc7e7f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/dr-who-police-box/static/js/2.9ddc7e7f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4be93280fcb7a69cfcd",
+    "url": "/dr-who-police-box/static/js/main.52e69cda.chunk.js"
+  },
+  {
+    "revision": "943a77acd39a6838de7b",
+    "url": "/dr-who-police-box/static/js/runtime-main.148a190a.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/dr-who-police-box/precache-manifest.d5312453a2fc15095b8e5d7852b010ca.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/dr-who-police-box/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
